c30ead8e53de85d83ba8e8bb17e803e97a8e5522
Harley Trung <harley@harley.io>
2016-03-12 10:47:08 +0700

    set up play/stop

--- flappybird1/GameViewController.swift
+++ flappybird1/GameViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class GameViewController: UIViewController {
+    var animator: UIDynamicAnimator!
+    var gravity = UIGravityBehavior()
+    var isPlaying = false
+    var originalPositionY: CGFloat!
+    
+    @IBOutlet weak var birdView: UIImageView!
+    @IBAction func onPlayTouched(sender: UIButton) {
+        if isPlaying {
+            isPlaying = false
+            sender.setTitle("Play", forState: .Normal)
+            animator.removeAllBehaviors()
+            birdView.frame.origin.y = originalPositionY
+        } else {
+            isPlaying = true
+            sender.setTitle("Stop", forState: .Normal)
+            setupGame()
+        }
+        
+    }
+    
+    func setupGame() {
+        gravity.addItem(birdView)
+        animator.addBehavior(gravity)
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-
         
+        animator = UIDynamicAnimator(referenceView: view)
+        originalPositionY = birdView.frame.origin.y
     }
     
     override func didReceiveMemoryWarning() {
